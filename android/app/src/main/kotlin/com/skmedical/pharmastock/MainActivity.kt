88c86927409f21d430862de4673f70e859bef9dc
afreedkhazii-prog <afreedkhazii@gmail.com>
2026-08-03 10:45:28 +0530

    Create android/app/src/main/kotlin/com/skmedical/pharmastock/MainActivity.kt

---
+++ android/app/src/main/kotlin/com/skmedical/pharmastock/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.skmedical.pharmastock
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
+Dependencies: camera, google_mlkit_text_recognition, sqflite, path_provider, image_picker
+Flutter SDK 3.0+
